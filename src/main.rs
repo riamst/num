9a2722cf9e18f7af61347b0ea135bc9e8c9257a2
riamst <31317128+riamst@users.noreply.github.com>
2023-07-10 20:56:53 +1000

    completed

--- src/main.rs
+++ src/main.rs
@@ -35,9 +35,23 @@ fn run(config: Config) -> Result<(), &'static str> {
 }
 
 fn parse_num(num_str: &str) -> Result<usize,&'static str> {
-    todo!()
+    let (prefix, rest) = num_str.split_at(2);
+    match prefix {
+        "0x" => usize::from_str_radix(rest,16),
+        "0b" => usize::from_str_radix(rest,2),
+        "0o" => usize::from_str_radix(rest, 8),
+        _ => num_str.parse(),
+    }.map_err(|_| "Error parsing input number")
 }
 
 fn convert(num: usize, out_base: String) -> Result<String,&'static str> {
-    todo!()
+    let a = &*out_base;
+    match a {
+        "d" => Ok(format!("{}", num)),
+        "b" => Ok(format!("{:b}", num)),
+        "o" => Ok(format!("{:o}", num)),
+        "x" => Ok(format!("{:x}", num)),
+        "X" => Ok(format!("{:X}", num)),
+        _ => Err("Invalid output base")
+    }
 }
